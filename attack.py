d88bb7f0f1a0a7563aec5092195008715e9ab2e2
Rexxagore-cyber <72860261+Rexxagore-cyber@users.noreply.github.com>
2020-10-18 19:08:37 +0700

    Update attack.py

--- attack.py
+++ attack.py
@@ -78,7 +78,7 @@ def usage():
 	print (''' \033[92m	Attack Server Script v.1 http://www.canyalcin.com/
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
-	usage : python3 hammer.py [-s] [-p] [-t]
+	usage : python3 attack.py [-s] [-p] [-t]
 	-h : help
 	-s : server ip
 	-p : port default 80
