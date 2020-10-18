8ac0e31894dcebb6a04f436103b09facfc6e5d53
Rexxagore-cyber <72860261+Rexxagore-cyber@users.noreply.github.com>
2020-10-18 19:00:44 +0700

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# python 3.3.2+ Hammer Dos Script v.1
+# python 3.3.2+ Attack Server Script v.1
 # by rexxagore
 # only for legal purpose
 
@@ -75,7 +75,7 @@ def dos2():
 
 
 def usage():
-	print (''' \033[92m	Hammer Dos Script v.1 http://www.canyalcin.com/
+	print (''' \033[92m	Attack Server Script v.1 http://www.canyalcin.com/
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
 	usage : python3 hammer.py [-s] [-p] [-t]
